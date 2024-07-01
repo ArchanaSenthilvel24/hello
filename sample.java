49c563676bbeb81f78f99aca55915688198e8b79
ArchanaSenthilvel24 <23203002@srcas.ac.in>
2024-07-01 11:59:03 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,10 @@
+class sample{
+  public static void main(String[]args)
+  {
+    System.out.println("Hello");
+    System.out.println("everyone");
+    System.out.println("welcome");
+    System.out.println("to");
+    System.out.println("M.Sc");
+  }
+}
